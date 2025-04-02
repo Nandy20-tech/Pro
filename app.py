d7037f39170dcc1f509585f2f67c117a39816b9d
Nandy20-tech <126004164@sastra.ac.in>
2025-04-02 17:45:13 +0000

    Fix port issue

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Flask is running on Render!"
+    return "Welcome to Patient database system!"
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 10000))  # Change default 10000 to 8080
